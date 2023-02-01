4f60f1cbbdcf0ec975df9af6d33ea599c8cabe4a
Michael Landis <malandis@gmail.com>
2023-02-01 09:44:49 -0800

    feat: rename package (#5)
    We use the name `momento-signer` for the official package name.

--- src/momento_signer/__init__.py
+++ src/momento_signer/__init__.py
@@ -2,4 +2,4 @@ import logging
 
 from .momento_signer import CacheOperation, MomentoSigner, SigningRequest
 
-logging.getLogger("momentosdk").addHandler(logging.NullHandler())
+logging.getLogger("momento-signer").addHandler(logging.NullHandler())
